609c48c54122adcfc4c611a4d73f67303bb8e2a3
심용현(팀) <yonghyeon.sim@pluglink.kr>
2022-11-10 01:38:44 +0900

    add ingress a service

--- src/main/java/com/example/ecr2/Ecr2Application.java
+++ src/main/java/com/example/ecr2/Ecr2Application.java
@@ -2,8 +2,10 @@ package com.example.ecr2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Ecr2Application {
 
     public static void main(String[] args) {
--- src/main/java/com/example/ecr2/controller/AClient.java
+++ src/main/java/com/example/ecr2/controller/AClient.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Component
 @FeignClient(value="example", url = "k8s-hello-testingr-db50395e52-753162095.ap-northeast-2.elb.amazonaws.com/A")
 public interface AClient {
     @GetMapping("/")
